58d43841288fe7ebc2e7f09571846addad1d1b83
Lokky <lokky2789@gmail.com>
2023-06-04 23:31:26 +0300

    Test added

---
+++ src/test/java/org/example/FileSorterTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class FileSorterTest {
+
+    @Test
+    void parseFile() {
+    }
+
+    @Test
+    void sortFileContent() {
+    }
+
+    @Test
+    void getCountOfRaws() {
+    }
+}
