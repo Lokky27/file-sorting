eba40d80f205c82e1e2bcc01075c1e3b335593e2
Vasilii Lokky <45848432+Lokky27@users.noreply.github.com>
2023-06-06 12:59:55 +0300

    Add tests, reading file, sorting of file content

--- src/main/java/org/example/FileSorter.java
+++ src/main/java/org/example/FileSorter.java
@@ -1,2 +1,14 @@
-package org.example;public class FileSorter {
+package org.example;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface FileSorter {
+
+    List<String> parseFile() throws IOException;
+
+    List<String> sortFileContent(List<String> parsedContent);
+
+    int getLinesOfFile() throws IOException;
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.impl.FileSorterImpl;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
 public class Main {
+    private static Properties properties = new Properties();;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            properties.load(new FileInputStream("src/main/resources/config.properties"));
+            FileSorter sorter = new FileSorterImpl(properties.getProperty("path"));
+            sorter.sortFileContent(sorter.parseFile()).forEach(System.out::println);
+
+
+        } catch (IOException e) {
+            System.err.println("File not Found!");
+        }
+
     }
 }
