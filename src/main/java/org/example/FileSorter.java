1787af1f0ddb3747a985e5e36ebbbbf732157f6a
Lokky <lokky2789@gmail.com>
2023-06-07 15:15:58 +0300

    Local changes

--- src/main/java/org/example/FileSorter.java
+++ src/main/java/org/example/FileSorter.java
@@ -1,2 +1,13 @@
-package org.example;public class FileSorter {
+package org.example;
+
+import java.util.List;
+
+public interface FileSorter {
+
+    List<String> parseFile(String path);
+
+    List<String> sortFileContent(List<String> parsedData);
+
+    int getCountOfRaws(String path);
+
 }
--- src/test/java/org/example/FileSorterTest.java
+++ src/test/java/org/example/FileSorterTest.java
@@ -1,10 +1,33 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FileSorterTest {
 
+    private static String path;
+    private static List<String> lines;
+
+    @BeforeAll
+    public static void init() {
+        Path pathToFile = Paths.get(path);
+        try {
+            BufferedReader bufferedReader = Files.newBufferedReader(pathToFile);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     @Test
     void parseFile() {
     }
